0ea546a2a74e65e16ee93abc67d73b6801112a42
Michael Draga <43150662+michaelb00m@users.noreply.github.com>
2022-04-17 14:54:54 +0200

    Update build.sh

--- web/build.sh
+++ web/build.sh
@@ -4,4 +4,7 @@ rm -rf dist/
 mkdir dist; 
 sed -e "s|%%FUNCTION_API_URL%%|${FUNCTION_API_URL}|" -e "s|%%PUBLIC_URL%%|${PUBLIC_URL}|" index.html > dist/index.html;
 
+echo ${FUNCTION_API_URL}
+echo ${PUBLIC_URL}
+
 cp -R assets dist/
